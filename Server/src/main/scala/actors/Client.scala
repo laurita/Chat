f8562f6dedd57c4918ff7117a7b21112e52a3f19
laura <laura.bledaite@gmail.com>
2014-03-11 14:56:28 -0400

    clean Client actor on server side

--- Server/src/main/scala/actors/Client.scala
+++ Server/src/main/scala/actors/Client.scala
@@ -14,6 +14,7 @@ object Client{
 
 class Client(clientSocket: Socket) extends Actor with ActorLogging {
 
+  // map from command strings to codes in bytes
   val commandCodes = Map(
     "login" -> 1.toByte,
     "send" -> 3.toByte,
@@ -21,153 +22,118 @@ class Client(clientSocket: Socket) extends Actor with ActorLogging {
     "receive" -> 5.toByte
   )
 
-  //log.info(self.path.name + " listening...")
+  // create data input and output streams on socket waiting for login connections
   val out = new DataOutputStream(new BufferedOutputStream(clientSocket.getOutputStream))
   val in = new DataInputStream(
     new BufferedInputStream(clientSocket.getInputStream))
 
-  //log.info("Receiving input...")
-
+  // get command and message in bytes
   val cmd = in.readByte()
-  val lenBytes = new Array[Byte](4)
-  in.read(lenBytes)
-  val len = Integer.parseInt(lenBytes.map(a => toBinary(a.toInt, 8)).mkString, 2)
-  val bytes = new Array[Byte](len)
-  in.read(bytes, 0, len)
-  val lst = bytes.toList
-  //log.info(s"cmdId: $cmd, length: $len, msg: $lst")
+  val loginName = readMessage(in)
 
-  self ! Parse(cmd, bytes)
+  // send a Parse message containing command and message to self
+  // self is in notLoggedIn mode
+  self ! Parse(cmd, loginName)
 
+  // first, receive behavior is notLoggedIn
   override def receive: Receive = notLoggedIn
+
 
   def notLoggedIn: Receive = {
 
-    case Parse(command, message) => {
-
-      log.info(s"$self received Parse($command, $message) when notLoggedIn")
+    case Parse(command, message) =>
+      log.info(s"got Parse($command, $message) when notLoggedIn")
 
       command match {
         // login
-        case 1 => {
-          log.info(s"matched command $command")
+        case 1 =>
           val username = byteArrayToString(message)
           val server = context.parent
-          log.info(s"sending Login($username) to $server")
           server ! Login(username)
-        }
 
         // send
-        case 3 => {
+        case 3 =>
           log.info("Parse(3, ...) not implemented")
-        }
 
-        case c => {
+        // unknown
+        case c =>
           log.info("unimplemented command "+ c)
-        }
       }
 
-    }
-
-    case UserCreated(name) => {
-
-      log.info(s"client $this got UserCreated($name)")
+    case UserCreated(name) =>
+      log.info(s"got UserCreated($name)")
       context.become(loggedIn(name))
-
-
+      // error code 0 is when OK
       val ats = Array[Byte](cmd, 0.toByte)
-      log.info("sending answer "+ ats.toList)
       out.write(ats)
       out.flush()
-
       self ! Listen
-    }
 
-    case UserExists => {
+    case UserExists =>
       // error code 1 is when user exists
       val ats = Array[Byte](cmd, 1.toByte)
       out.write(ats)
       out.flush()
-    }
   }
 
   def loggedIn(name: String): Receive = {
 
-    case Parse(command, message) => {
+    case Parse(command, message) =>
 
       val lst = message.toList
-      log.info(s"Client $self got message Parse($command, $lst)")
-      log.info("message length is "+ message.length)
+      log.info(s"got message Parse($command, $lst)")
       command match {
         // login
-        case 1 => {
+        case 1 =>
           // already logged in
           log.info("already logged in")
-        }
 
         // send
-        case 3 => {
+        case 3 =>
           log.info("forwarding to others...")
-          val from = name
-          context.system.actorSelection("user/server") ! ForwardAll(from, message)
-        }
+          context.system.actorSelection("user/server") ! ForwardAll(name, message)
 
         // logout
-        case 4 => {
-          log.info(s"Client sends Server Logout($name)")
+        case 4 =>
+          log.info(s"sends Server Logout($name)")
           context.system.actorSelection("user/server") ! Logout(name, out)
           context.become(notLoggedIn)
-        }
       }
 
-    }
-
-    case Listen => {
-      //log.info(self +" received "+ Listen)
+    case Listen =>
       if (in.available() != 0) {
         val cmd = in.readByte()
-        val lenBytes = new Array[Byte](4)
-        in.read(lenBytes)
-        val len = Integer.parseInt(lenBytes.map(a => toBinary(a.toInt, 8)).mkString, 2)
-        val bytes = new Array[Byte](len)
-        in.read(bytes, 0, len)
-        val lst = bytes.toList
-        log.info(s"cmdId: $cmd, length: $len, msg: $lst")
-
+        val bytes = readMessage(in)
         self ! Parse(cmd, bytes)
       }
       self ! Listen
-    }
 
-    case Message(bytes: Array[Byte]) => {
-      val lst = bytes.toList
-      log.info(s"Client $self got message: $lst")
-      log.info("message length is "+ bytes.length)
-      log.info("Client is sending it through socket...")
-
+    case Message(bytes: Array[Byte]) =>
+      log.info("got message: "+ bytes.toList)
+      // send to backend through TCP socket
       out.write(bytes)
       out.flush()
-    }
-
   }
 
+  //-------------------------------------- HELPERS -------------------------------------------------------------------//
+
+  // converts integer to 8 bit string
   private def toBinary(i: Int, digits: Int = 8) =
     String.format("%" + digits + "s", i.toBinaryString).replace(' ', '0')
 
+  // makes string from array of bytes
   private def byteArrayToString(byteArray: Array[Byte]): String = {
     byteArray.toList.map(x => x.toChar).mkString
   }
 
-  private def splitToStringsOfLen(str: String, len: Int): List[String] = {
-    def rec(str: String, acc: List[String]): List[String] = {
-      str match {
-        case "" => acc
-        case string => {
-          val tpl = string.splitAt(string.length - len)
-          rec(tpl._1, tpl._2 :: acc)
-        }
-      }
-    }
-    rec(str, List())
+  // reads 4 bytes from given input stream to get length N of loginName
+  // returns next N bytes (loginName)
+  private def readMessage(in: DataInputStream): Array[Byte] = {
+    val lenBytes = new Array[Byte](4)
+    in.read(lenBytes)
+    val len = Integer.parseInt(lenBytes.map(a => toBinary(a.toInt, 8)).mkString, 2)
+    val bytes = new Array[Byte](len)
+    in.read(bytes, 0, len)
+    bytes
   }
 }
